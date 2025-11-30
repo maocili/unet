6bc48635910280e768b540fd55d296056b9ffc22
xuxifeng <993marsxu@gmail.com>
2025-11-30 22:19:19 +0100

    update softmax_mse_loss

--- mean_teacher/losses.py
+++ mean_teacher/losses.py
@@ -12,19 +12,27 @@ from torch.nn import functional as F
 from torch.autograd import Variable
 
 
-def softmax_mse_loss(input_logits, target_logits):
-    """Takes softmax on both sides and returns MSE loss
+# def softmax_mse_loss(input_logits, target_logits):
+#     """Takes softmax on both sides and returns MSE loss
 
-    Note:
-    - Returns the sum over all examples. Divide by the batch size afterwards
-      if you want the mean.
-    - Sends gradients to inputs but not the targets.
-    """
+#     Note:
+#     - Returns the sum over all examples. Divide by the batch size afterwards
+#       if you want the mean.
+#     - Sends gradients to inputs but not the targets.
+#     """
+#     assert input_logits.size() == target_logits.size()
+#     input_softmax = F.softmax(input_logits, dim=1)
+#     target_softmax = F.softmax(target_logits, dim=1)
+#     num_classes = input_logits.size()[1]
+#     return F.mse_loss(input_softmax, target_softmax, size_average=False) / num_classes
+
+def softmax_mse_loss(input_logits, target_logits):
+
     assert input_logits.size() == target_logits.size()
     input_softmax = F.softmax(input_logits, dim=1)
     target_softmax = F.softmax(target_logits, dim=1)
-    num_classes = input_logits.size()[1]
-    return F.mse_loss(input_softmax, target_softmax, size_average=False) / num_classes
+    mse_loss = F.mse_loss(input_softmax, target_softmax, reduction='mean')
+    return mse_loss
 
 
 def softmax_kl_loss(input_logits, target_logits):
